dab931bf1e1415e0971b3ea3a07639d61ce11cc0
AmazonPython <amazon3@foxmail.com>
2022-02-18 21:30:31 +0800

    API basic config ✅

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -69,6 +69,9 @@ class Kernel extends HttpKernel
         // API 中间件组，应用于 routes/api.php 路由文件，
         // 在 RouteServiceProvider 中设定
         'api' => [
+            // 设置默认 Accept 头为 application/json
+            \App\Http\Middleware\AcceptHeader::class,
+
             // 使用别名来调用中间件
             // 请见：https://learnku.com/docs/laravel/{{doc_version}}/middleware#为路由分配中间件
             'throttle:api',
