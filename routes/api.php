86aa22b98ea7db7bafcd0543a1bc76e2c1f12fba
AmazonPython <amazon3@foxmail.com>
2022-02-27 20:58:25 +0800

    Build SMS verification code interface.✅

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
-Route::prefix('v1')->name('api.v1.')->group(function () {
-
+Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function () {
+    // 短信验证码
+    Route::post('verificationCodes', 'VerificationCodesController@store')->name('verificationCodes.store');
 });
