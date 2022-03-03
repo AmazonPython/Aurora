c6c1dd252f72f256b6701a50f585ce9239b06c47
AmazonPython <amazon3@foxmail.com>
2022-03-03 21:18:25 +0800

    Added captchas API route, and modified the previous verification logic. 👽️

--- app/Http/Requests/Api/VerificationCodeRequest.php
+++ app/Http/Requests/Api/VerificationCodeRequest.php
@@ -2,33 +2,21 @@
 
 namespace App\Http\Requests\Api;
 
-use Illuminate\Foundation\Http\FormRequest;
-
 class VerificationCodeRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
-            'phone' => [
-                'required',
-                'regex:/^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199)\d{8}$/',
-                'unique:users'
-            ]
+            'captcha_key' => 'required|string',
+            'captcha_code' => 'required|string',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'captcha_key' => '图片验证码 key',
+            'captcha_code' => '图片验证码',
         ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,9 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function () {
 
         // 用户注册
         Route::post('users', 'UsersController@store')->name('users.store');
+
+        // 图片验证码
+        Route::post('captchas', 'CaptchasController@store')->name('captchas.store');
     });
 
     Route::middleware('throttle:' . config('api.rate_limits.access'))->group(function () {
