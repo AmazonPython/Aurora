24d6531fa0e0485cbf3e4f7a3a77c3ecf914db3e
AmazonPython <amazon3@foxmail.com>
2022-03-05 11:14:55 +0800

    Build wechat social login interface. ✅

--- routes/api.php
+++ routes/api.php
@@ -23,11 +23,15 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function () {
         // 短信验证码
         Route::post('verificationCodes', 'VerificationCodesController@store')->name('verificationCodes.store');
 
+        // 图片验证码
+        Route::post('captchas', 'CaptchasController@store')->name('captchas.store');
+
         // 用户注册
         Route::post('users', 'UsersController@store')->name('users.store');
 
-        // 图片验证码
-        Route::post('captchas', 'CaptchasController@store')->name('captchas.store');
+        // 第三方登录
+        Route::post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
+            ->where('social_type', 'wechat')->name('socials.authorizations.store');
     });
 
     Route::middleware('throttle:' . config('api.rate_limits.access'))->group(function () {
