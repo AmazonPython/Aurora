39da3fd1b61b52bef9858340ef9b7fe66deaecfc
AmazonPython <amazon3@foxmail.com>
2022-03-09 21:37:47 +0800

    Added topic list interface, and new dependencies. ✅ ➕

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -2,13 +2,44 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\User;
 use App\Models\Topic;
 use Illuminate\Http\Request;
 use App\Http\Resources\TopicResource;
 use App\Http\Requests\Api\TopicRequest;
+use Spatie\QueryBuilder\QueryBuilder;
+use Spatie\QueryBuilder\AllowedFilter;
 
 class TopicsController extends Controller
 {
+    public function userIndex(User $user, Request $request)
+    {
+        $query = $user->topics()->getQuery();
+
+        $topics = QueryBuilder::for($query)
+            ->allowedIncludes(['user', 'category'])
+            ->allowedFilters([
+                'title',
+                AllowedFilter::exact('category_id'),
+                AllowedFilter::scope('withOrder')->default('recentReplied'),
+            ])->paginate();
+
+        return TopicResource::collection($topics);
+    }
+
+    public function index(Request $request, Topic $topic)
+    {
+        $topics = QueryBuilder::for(Topic::class)
+            ->allowedIncludes('user', 'category')
+            ->allowedFilters([
+                'title',
+                AllowedFilter::exact('category_id'),
+                AllowedFilter::scope('withOrder')->default('recentReplied'),
+            ])->paginate();
+
+        return TopicResource::collection($topics);
+    }
+
     public function store(TopicRequest $request, Topic $topic)
     {
         $topic->fill($request->all());
